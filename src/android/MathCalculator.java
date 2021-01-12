ab9267c042fdc1b5560c6e359dde111d9f35cb04
sharadk <sharad@datalogysoftware.com>
2021-01-12 16:49:21 +0530

    new commint update

--- src/android/MathCalculator.java
+++ src/android/MathCalculator.java
@@ -15,7 +15,13 @@ public class MathCalculator extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-        if(action.equals("add"))
+           if (action.equals("coolMethod")) {
+             String message = args.getString(0);
+             this.coolMethod(message, callbackContext);
+             return true;
+         }
+
+        else if(action.equals("add"))
         {
             this.add(args,callbackContext);
             return true;
@@ -25,22 +31,18 @@ public class MathCalculator extends CordovaPlugin {
             this.substract(args,callbackContext);
             return true;
         }
-        // if (action.equals("coolMethod")) {
-        //     String message = args.getString(0);
-        //     this.coolMethod(message, callbackContext);
-        //     return true;
-        // }
+      
 
         return false;
     }
 
-    // private void coolMethod(String message, CallbackContext callbackContext) {
-    //     if (message != null && message.length() > 0) {
-    //         callbackContext.success(message);
-    //     } else {
-    //         callbackContext.error("Expected one non-empty string argument.");
-    //     }
-    // }
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
 
     private void add(JSONArray args, CallbackContext callbackContext)
     {
--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -1,15 +1,15 @@
 var exec = require('cordova/exec');
 
-// exports.coolMethod = function (arg0, success, error) {
-//     exec(success, error, 'MathCalculator', 'coolMethod', [arg0]);
-// };
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'coolMethod', [arg0]);
+};
 
-module.exports.add = function (arg0,success,error)
+exports.add = function (arg0,success,error)
 {
     exec(success,error,'MathCalculator','add',[arg0]);
 }
 
-module.exports.substract = function (arg0,success,error)
+exports.substract = function (arg0,success,error)
 {
     exec(success,error,'MathCalculator','substract',[arg0]);
 }
